34cd7b52ce81c81875b501e19942162dba448cc4
Minh Tran <trcongminh.261002@gmail.com>
2025-07-03 09:26:54 +0700

    Refactor Home and Login pages to improve session handling and UI. Integrated sign-out functionality in Home page and replaced login UI with a dedicated LoginForm component.

---
+++ app/(auth)/login/_components/LoginForm.tsx
@@ -0,0 +1,80 @@
+"use client"
+
+import { useTransition } from 'react'
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { authClient } from '@/providers/auth-client'
+import { ErrorContext } from 'better-auth/react'
+import { Loader2, MailIcon } from 'lucide-react'
+import Image from 'next/image'
+import { toast } from 'sonner'
+
+const LoginForm = () => {
+
+    const [isGooglePending, startGoogleTransition] = useTransition();
+
+    async function handleGoogleSignIn() {
+        startGoogleTransition(async () => {
+            await authClient.signIn.social({
+                provider: "google",
+                callbackURL: "/",
+                fetchOptions: {
+                    onSuccess: () => {
+                        toast.success("Login successful, redirecting...");
+                    },
+                    onError: (error: ErrorContext) => {
+                        toast.error(error.error?.message || "Login failed, please try again.");
+                    },
+                },
+            });
+        })
+    }
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle className='text-xl'>Welcome back !</CardTitle>
+                <CardDescription>Login with Email or Google Account to continue</CardDescription>
+            </CardHeader>
+            <CardContent className='flex flex-col gap-4'>
+                <Button onClick={handleGoogleSignIn}
+                    className='w-full cursor-pointer'
+                    variant='outline'
+                    disabled={isGooglePending}
+                >
+
+                    {isGooglePending ? (
+                        <Loader2 className='size-4 mr-2 animate-spin' />
+                    ) : (
+                        <>
+                            <Image src="/images/google.png" alt="google" width={20} height={20} className='mr-2' />   Continue with Google
+                        </>
+                    )}
+                </Button>
+
+                <div className='relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border'>
+                    <span className='relative z-10 bg-card px-2 text-muted-foreground'>Or continue with</span>
+                </div>
+
+                <div className="grid gap-3">
+                    <div className="grid gap-2">
+                        <Label htmlFor='email'>Email</Label>
+                        <Input
+                            id='email'
+                            placeholder='Enter your email'
+                            type='email'
+                        />
+                    </div>
+                </div>
+
+                <Button className='w-full cursor-pointer'>
+                    <MailIcon className="size-4 mr-2" />
+                    Continue with Email
+                </Button>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default LoginForm
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,37 @@
-import { ToggleTheme } from '@/components/ui/theme-toggle'
-import { auth } from '@/lib/auth'
-import { headers } from 'next/headers'
+"use client"
 
-const HomePage = async () => {
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  });
+import { Button } from '@/components/ui/button'
+import { ToggleTheme } from '@/components/ui/theme-toggle'
+import { authClient } from '@/providers/auth-client'
+import { ErrorContext } from 'better-auth/react'
+import { toast } from 'sonner'
+
+const HomePage = () => {
+  const {
+    data: session,
+  } = authClient.useSession()
+
+  async function handleSignOut() {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          toast.success("Signed out successfully")
+        },
+        onError: (error: ErrorContext) => {
+          toast.error(error.error?.message || "Failed to sign out")
+        },
+      },
+    })
+  }
+
   return (
     <div>
-      {session ? <p>{session?.user.name} is logged in</p> : <p>Logged out</p>}
+      {session ? (
+        <>
+          <p>{session?.user.name} is logged in</p>
+          <Button onClick={handleSignOut}>Sign out</Button>
+        </>
+      ) : <p>Logged out</p>}
       <ToggleTheme />
     </div>
   )
